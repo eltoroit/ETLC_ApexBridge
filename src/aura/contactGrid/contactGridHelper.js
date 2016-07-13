94ad62132e8720ccc55b93b9c4ce9e627c6d23dc
aperez <aperez@salesforce.com>
2016-07-13 12:57:01 -0700

    New Version, more samples.

--- src/aura/contactGrid/contactGridHelper.js
+++ src/aura/contactGrid/contactGridHelper.js
@@ -1,5 +1,15 @@
 ({
     findContacts : function(component, helper) {
+        // Switch these comments around to see the other behaviour
+        // helper.findContactsUniqueController(component, helper);
+		helper.findContactsReuseController(component, helper);
+    },
+    updateContacts : function(component, helper, records) {
+        // Switch these comments around to see the other behaviour
+        // helper.updateContactsUniqueController(component, helper, records);
+        helper.updateContactsReuseController(component, helper, records);
+    },
+    findContactsUniqueController : function(component, helper) {
         var accountId = component.get("v.accountId");
         var apexBridge = component.find("ApexBridge");
         // apexBridge.set("v.debugClient", true);
@@ -18,7 +28,27 @@
             }
         });
     },
-    updateContacts : function(component, helper, records) {
+    findContactsReuseController : function(component, helper) {
+        var accountId = component.get("v.accountId");
+        var apexBridge = component.find("ApexBridge");
+        // apexBridge.set("v.debugClient", true);
+        // apexBridge.set("v.debugServer", true);
+        apexBridge.callApex({
+            component: component, 
+            data: {
+                operation: "ContactsController",
+                input: {
+                    mode: "QueryByAccountId",
+                    accountId: accountId
+                }
+            },
+            callBackMethod: function (data) {
+                component.set("v.contactsChanged", {});
+                component.set("v.contacts", data.output);
+            }
+        });
+    },
+    updateContactsUniqueController : function(component, helper, records) {
         var apexBridge = component.find("ApexBridge");
         // apexBridge.set("v.debugClient", true);
         // apexBridge.set("v.debugServer", true);
@@ -26,6 +56,26 @@
             component: component, 
             data: {
                 operation: "updateContacts",
+                records: records
+            },
+            callBackMethod: function (data) {
+                helper.findContacts(component, helper);
+                component.find("btnSaveContacts").set("v.disabled", true);
+            }
+        });
+        
+    },
+    updateContactsReuseController : function(component, helper, records) {
+        var apexBridge = component.find("ApexBridge");
+        // apexBridge.set("v.debugClient", true);
+        // apexBridge.set("v.debugServer", true);
+        apexBridge.callApex({
+            component: component, 
+            data: {
+                operation: "ContactsController",
+                input: {
+                    mode: "UpdateChangedContacts"
+                },
                 records: records
             },
             callBackMethod: function (data) {
